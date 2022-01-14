f53ec28967daf4e5730b0d1cfe0f80cafb31f2fa
Achyut Krishna Sai Adiraju <achyutkrishna1134@gmail.com>
2022-01-14 18:25:07 +0530

    first commit

---
+++ backend/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+   // mongoURI: "mongodb+srv://admin:Jake@12345@cluster0-8lbwj.mongodb.net/timer?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://db_admin:Achyutkrishna@2001@merncluster.8irh5.mongodb.net/myFirstDatabase?retryWrites=true&w=majoritymongodb+srv://admin:Jake@12345@cluster0-8lbwj.mongodb.net/timer?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
